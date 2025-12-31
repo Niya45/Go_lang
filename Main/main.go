18bb3e5e95f0c33e77bac66a37e5948e6c6e6c95
Niya <niya123op@gmail.com>
2025-12-31 06:40:47 +0530

    added notes and some lines of code

--- Main/main.go
+++ Main/main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("This is a test run")
+	// declare variables:
+	var name string = "sam" // use in bit operations
+	new_name := "jack" // to initialize a local variable
+	// FMT package:
+	fmt.Printf("My name used to be %v but now it's %v\n", name, new_name)
+	// escape sequences:
+	fmt.Printf("Line one \"backslash\"=\\ \nLine two \tTabspace \nLine three\v Vertical space\n")
 }
