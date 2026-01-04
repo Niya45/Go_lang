5fa630a1178215f11eec2d45c611bf6c02719966
Niya <niya123op@gmail.com>
2026-01-04 19:17:49 +0530

    Added notes on functions and control structures and updated some notes

--- Main/functions.go
+++ Main/functions.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"errors"
+	"os"
 )
 
 func parentFunction() {
@@ -11,8 +12,13 @@ func parentFunction() {
 	c,d := returnT("string1", "string2")
 	fmt.Println("returning two strings : ",c,d)
 
-	// 4. blank identifier : ignores a return value
-	result, _ := mulReturn()
+	// 4. blank identifier 
+	result, _ := mulReturn(4.0,5.0) // ignore a value
+	fmt.Println(result)
+	
+	deferOrder()
+	deleteFiles("Filename", true)
+	readFile()
 }
 
 // 1. naming multiple parameters consicutively
@@ -35,3 +41,42 @@ func mulReturn(e,f float64) (float64, error) {
 	}
 }
 
+// 5. Variadic functions
+func sum(nums ...int) int {
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
+
+// 6. Defer
+
+func readFile() {
+	f,err := os.Open("Variables.md")
+	if err != nil {
+		return
+	}
+	defer f.Close() //executes when return gets called
+	// defer runs after the return but finishes before function exits
+}
+
+// LIFO order (last deffered, first executed)
+func deferOrder() {
+	defer fmt.Println("Fourth")
+	defer fmt.Println("Third")
+	fmt.Println("First") // executes first
+	defer fmt.Println("Second")
+}
+
+
+// 7. Panic (Rule of thump : DON't CALL PANIC unless necessary)
+
+func deleteFiles(fileName string, del bool) error {
+	if del == true {
+		//panic("You Can't delete the file")
+		return errors.New("The file cannot be deleted")
+	} else {
+		return nil
+	}
+}
--- Main/main.go
+++ Main/main.go
@@ -1,6 +1,8 @@
 package main
 
 func main() {
-	parentFuncPackages()//packages.go
-	parentFuncVars()
+	//parentFuncPackages()// packages.go
+	//parentFuncVars() // vars_dt.go
+	//parentFunction() // functions.go
+	parentControl()
 }
